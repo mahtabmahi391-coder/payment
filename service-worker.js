04d2e82278775bf31e0796442131095a963ee76e
mahtabmahi391-coder <mahtabmahi391@gmail.com>
2026-09-08 03:42:39 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'mahi-payment-v1';
+// Change the version number (e.g., v1 to v2) every time you update the site
+const CACHE_NAME = 'mahi-payment-v2'; 
+
 const assets = [
   './',
   './index.html',
@@ -8,9 +10,20 @@ const assets = [
 ];
 
 self.addEventListener('install', e => {
+  self.skipWaiting(); // Forces the new service worker to take over immediately
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
